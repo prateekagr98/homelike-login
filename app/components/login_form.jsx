6ac7793c61fb8f35919cb54a190b7bdfe1a65596
Prateek Agarwal <prateekagr98@gmail.com>
2017-10-18 23:55:36 +0530

    decrease font size

--- app/components/login_form.jsx
+++ app/components/login_form.jsx
@@ -5,11 +5,11 @@ import React from 'react';
 const LoginForm = () => (
 	<form>
 	  <div className="form-group">
-	    <label for="exampleInputEmail1" className="text-secondary">Email address</label>
+	    <label for="exampleInputEmail1" className="text-secondary u-t-base">Email address</label>
 	    <input type="email" className="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required />
 	  </div>
 	  <div className="form-group">
-	    <label for="exampleInputPassword1" className="text-secondary">Password</label>
+	    <label for="exampleInputPassword1" className="text-secondary u-t-base">Password</label>
 	    <input type="password" className="form-control form-control-sm" id="exampleInputPassword1" placeholder="Password" required />
 	  </div>
 	  <div className="text-right">
--- app/components/signup_form.jsx
+++ app/components/signup_form.jsx
@@ -5,30 +5,30 @@ import React from 'react';
 const SignupForm = () => (
 	<form>
 		<div className="form-group">
-	    <label for="firstName" className="text-secondary">First Name</label>
+	    <label for="firstName" className="text-secondary u-t-base">First Name</label>
 	    <input type="text" className="form-control form-control-sm" id="firstName" placeholder="Enter first name" required />
 	  </div>
 	  <div className="form-group">
-	    <label for="lastName" className="text-secondary">Last Name</label>
+	    <label for="lastName" className="text-secondary u-t-base">Last Name</label>
 	    <input type="text" className="form-control form-control-sm" id="lastName" placeholder="Enter second name" required />
 	  </div>
 	  <div className="form-group">
-	    <label for="exampleInputEmail1" className="text-secondary">Email address</label>
+	    <label for="exampleInputEmail1" className="text-secondary u-t-base">Email address</label>
 	    <input type="email" className="form-control form-control-sm" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" required />
 	  </div>
 	  <div className="form-group">
-	    <label for="exampleInputPassword1" className="text-secondary">Password</label>
+	    <label for="exampleInputPassword1" className="text-secondary u-t-base">Password</label>
 	    <input type="password" className="form-control form-control-sm" id="exampleInputPassword1" placeholder="Password" required />
 	  </div>
 	  <div className="form-group">
-	    <label for="user_type" className="text-secondary">User type</label>
+	    <label for="user_type" className="text-secondary u-t-base">User type</label>
 	    <select className="form-control" id="user_type">
 	      <option>Private Tenant</option>
 	      <option>Apartment Owner/Tenant</option>
 	    </select>
 	  </div>
 	  <div className="form-group">
-	    <label for="language" className="text-secondary">Language</label>
+	    <label for="language" className="text-secondary u-t-base">Language</label>
 	    <select className="form-control" id="language">
 	      <option>English</option>
 	      <option>Deutsch</option>
